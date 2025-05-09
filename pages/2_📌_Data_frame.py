53a53822ee337e3d9bdbfb33a01b5f570e86538d
lina marin j <linamarin2011@gmail.com>
2025-05-09 17:07:10 -0500

    correción ruta csv

--- pages/2_📌_Data_frame.py
+++ pages/2_📌_Data_frame.py
@@ -7,7 +7,7 @@ st.set_page_config(page_icon="📌", layout="wide")
 st.title("Dataframe")
 
 df = pd.read_csv(
-    "pages\static\dataset\Defunciones_ocurridas_en__en_el_Hospital_General_de_Medell_n_20250502.csv"
+    "pages/static/dataset/Defunciones_ocurridas_en__en_el_Hospital_General_de_Medell_n_20250502.csv"
 )
 st.markdown(
     '<h2 style="color: #0066cc;">Defunciones ocurridas en el Hospital General de Medellín 2022-2023</h3>',
