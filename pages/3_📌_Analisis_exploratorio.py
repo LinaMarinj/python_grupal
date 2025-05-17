08e460270052b65568e7810e0cc158d19e029ccf
MarianaDS432 <marianariveraperez21@gmail.com>
2025-05-16 20:18:57 -0500

    se renombra un file y se crea uno nuevo

---
+++ pages/3_📌_Analisis_exploratorio.py
@@ -0,0 +1,26 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import plotly.express as px
+
+# Configuración de la página
+st.set_page_config(page_icon="📌", layout="wide")
+
+st.title("Gráficos")
+
+
+data = pd.DataFrame({
+    'Categoría': ['A', 'B', 'C', 'D', 'E'],  
+    'Valor': [10, 24, 36, 28, 15],
+    'Grupo': ['Edad', 'Genero', 'Municipio', 'estado civil', 'causa'] 
+})
+
+#t.subheader("python_grupal/pages/static/dataset/Defunciones_ocurridas_en__en_el_Hospital_General_de_Medell_n_20250502.csv")
+plt.figure(figsize=(8, 6))
+plt.bar(data['Categoría'], data['Valor'])
+plt.xlabel('Categoría')
+plt.ylabel('Valor')
+plt.title('Gráfico de Barras de las Defunciones ocurridas en el hospital')
+st.pyplot(plt)
+
